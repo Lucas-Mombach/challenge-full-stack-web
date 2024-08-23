0e44b2cc8926d57fa968ca72d09e2143d4876cc6
Lucas Mombach <lucasmmombach@gmail.com>
2024-08-23 17:15:02 -0300

    Add registration form with fields for Nome, Email, RA, and CPF

---
+++ src/client/matriculas/src/components/StudentForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container>
+    <v-form v-model="valid" @submit.prevent="submitForm">
+      <v-container cols="12" class="d-flex align-center justify-center">
+        <v-responsive max-width="400">
+          <v-text-field
+            label="Nome"
+            rounded="lg"
+            variant="solo-filled"
+            flat
+            hide-details
+            single-line
+            v-model="form.name"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+
+      <v-container cols="12" class="d-flex align-center justify-center">
+        <v-responsive max-width="400">
+          <v-text-field
+            label="Email"
+            rounded="lg"
+            variant="solo-filled"
+            flat
+            hide-details
+            single-line
+            v-model="form.email"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+
+      <v-container cols="12" class="d-flex align-center justify-center">
+        <v-responsive max-width="400">
+          <v-text-field
+            label="RA"
+            rounded="lg"
+            variant="solo-filled"
+            flat
+            hide-details
+            single-line
+            v-model="form.ra"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+
+      <v-container cols="12" class="d-flex align-center justify-center">
+        <v-responsive max-width="400">
+          <v-text-field
+            label="CPF"
+            rounded="lg"
+            variant="solo-filled"
+            flat
+            hide-details
+            single-line
+            v-model="form.cpf"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+
+      <v-container class="d-flex align-center justify-center">
+        <v-responsive max-width="160">
+          <v-btn variant="tonal" type="submit" block>
+            Registrar
+          </v-btn>
+        </v-responsive>
+      </v-container>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+
+</script>
